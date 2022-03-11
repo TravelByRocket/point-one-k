473e83f9283b6f0a5a94b161ac2cd0cb0df8e92d
Bryan Costanza <29235268+TravelByRocket@users.noreply.github.com>
2022-03-11 15:46:30 -0700

    Remove unused label and icon

--- PointOneK/PointOneK/Activities/Projects/ItemRowView.swift
+++ PointOneK/PointOneK/Activities/Projects/ItemRowView.swift
@@ -11,29 +11,6 @@ struct ItemRowView: View {
     @ObservedObject var project: Project
     @ObservedObject var item: Item
 
-    var icon: some View {
-        if item.completed {
-            return Image(systemName: "checkmark.circle")
-                .foregroundColor(Color(project.projectColor))
-        } else if item.priority == 3 {
-            return Image(systemName: "exclamationmark.triangle")
-                .foregroundColor(Color(project.projectColor))
-        } else {
-            return Image(systemName: "checkmark.circle")
-                .foregroundColor(.clear)
-        }
-    }
-
-    var label: Text {
-        if item.completed {
-            return Text("\(item.itemTitle), completed.")
-        } else if item.priority == 3 {
-            return Text("\(item.itemTitle), high priority.")
-        } else {
-            return Text(item.itemTitle)
-        }
-    }
-
     var body: some View {
         NavigationLink(destination: EditItemView(item: item)) {
             HStack {
