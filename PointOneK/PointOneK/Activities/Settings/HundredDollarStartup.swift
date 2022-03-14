3771b4f669826027dbda6e1f0da9696c58e09625
Bryan Costanza <29235268+TravelByRocket@users.noreply.github.com>
2022-03-13 20:20:29 -0600

    Expand $100 Startup template description

--- PointOneK/PointOneK/Activities/Settings/HundredDollarStartup.swift
+++ PointOneK/PointOneK/Activities/Settings/HundredDollarStartup.swift
@@ -8,6 +8,7 @@
 import Foundation
 import CoreData
 
+// swiftlint:disable:next function_body_length
 func makeHundredDollarStartup(_ dataController: DataController) {
     let managedObjectContext = dataController.container.viewContext
 
@@ -21,25 +22,50 @@ func makeHundredDollarStartup(_ dataController: DataController) {
     ease.project = project
     ease.title = "Ease"
     ease.indicator = "e"
-    ease.note = "How easy is this project? Easier the better! And don't try too hard, the easy way is hard enough"
+    // swiftlint:disable line_length
+    ease.note = """
+        How easy is this project? Easier the better! And don't try too hard, the easy way is hard enough
+        4) I can do this task in my sleep, like singing happy birthday. Or it takes little time or resources. <10s of hours for a project.
+        3) I have to gather resources, maybe depend on others a little bit, or put in some effort to completing the task. 100s of hours for a project.
+        2) This will take substantial effort, studying, resources, or coordintation. 1000s of hours for a project.
+        1) Huge amounts of resources. Many 1000s of hours for a project.
+        """
 
     let impact = Quality(context: managedObjectContext)
     impact.project = project
     impact.title = "Impact"
     impact.indicator = "i"
-    impact.note = "How much of an impact will this have on customers? Your community? The world?"
+    impact.note = """
+        How much of an impact will this have on customers? Your community? The world?
+        4) Huge; lots of people or very impactful on some
+        3) Good; noticed by many or helpful in some ways
+        2) Some; nothing amazing but appreciable and positive
+        1) Little; not much going for it in this category
+        """
 
     let vision = Quality(context: managedObjectContext)
     vision.project = project
     vision.title = "Vision"
     vision.indicator = "v"
-    vision.note = "How closely does this align with your vision of your ideak work?"
+    vision.note = """
+        How closely does this align with your vision of your ideal work?
+        4) It's exactly what I want to do for as long I can imagine
+        3) Has elements I would change or I could see doing this a few years
+        2) It has elements of my perfect vision and is a stepping stone
+        1) There's not much I am excited to be getting experience in
+        """
 
     let profitability = Quality(context: managedObjectContext)
     profitability.project = project
     profitability.title = "Profitability"
     profitability.indicator = "p"
-    profitability.note = "How much moola potential is there here?"
+    profitability.note = """
+        How much moola potential is there here?
+        4) Pays my salary
+        3) Pays for rent
+        2) Pays for utilities
+        1) Pays for really fancy ketchup
+        """
 
     dataController.save()
 }
