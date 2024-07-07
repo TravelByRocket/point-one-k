e4bae1ffa01aa14afdc18f8621f947a224910506
Bryan Costanza <29235268+TravelByRocket@users.noreply.github.com>
2024-07-07 12:23:02 -0600

    add tests

--- PointOneK/PointOneKTests/ProjectTests.swift
+++ PointOneK/PointOneKTests/ProjectTests.swift
@@ -66,4 +66,46 @@ final class ProjectTests: BaseTestCase {
         #expect((try? context.fetchCount(FetchDescriptor<ScoreV2>())) == 0)
         #expect((try? context.fetchCount(FetchDescriptor<QualityV2>())) == 0)
     }
+
+    @MainActor @Test func testAddItem() {
+        let project = ProjectV2()
+        project.addItem(titled: "Item Title")
+        context.insert(project)
+
+        #expect((try? context.fetchCount(FetchDescriptor<ProjectV2>())) == 1)
+        #expect((try? context.fetchCount(FetchDescriptor<ItemV2>())) == 1)
+    }
+
+    @MainActor @Test func testAddQuality() {
+        let project = ProjectV2()
+        project.addQuality()
+        context.insert(project)
+
+        #expect((try? context.fetchCount(FetchDescriptor<ProjectV2>())) == 1)
+        #expect((try? context.fetchCount(FetchDescriptor<QualityV2>())) == 1)
+    }
+
+    @MainActor @Test func test_givenProject_whenAddItemThenQuality_thenScoreCreated() {
+        let project = ProjectV2()
+        project.addItem()
+        project.addQuality()
+        context.insert(project)
+
+        #expect((try? context.fetchCount(FetchDescriptor<ProjectV2>())) == 1)
+        #expect((try? context.fetchCount(FetchDescriptor<ItemV2>())) == 1)
+        #expect((try? context.fetchCount(FetchDescriptor<ScoreV2>())) == 1)
+        #expect((try? context.fetchCount(FetchDescriptor<QualityV2>())) == 1)
+    }
+
+    @MainActor @Test func test_givenProject_whenAddQualityThenItem_thenScoreCreated() {
+        let project = ProjectV2()
+        project.addQuality()
+        project.addItem()
+        context.insert(project)
+
+        #expect((try? context.fetchCount(FetchDescriptor<ProjectV2>())) == 1)
+        #expect((try? context.fetchCount(FetchDescriptor<ItemV2>())) == 1)
+        #expect((try? context.fetchCount(FetchDescriptor<ScoreV2>())) == 1)
+        #expect((try? context.fetchCount(FetchDescriptor<QualityV2>())) == 1)
+    }
 }
