f2e8357ccc7fec845ba07a11d0f7fb98c24ce36d
Bryan Costanza <29235268+TravelByRocket@users.noreply.github.com>
2024-07-15 19:21:19 -0600

    need observed object

--- PointOneK/PointOneK/Activities/Home/ProjectRowView.swift
+++ PointOneK/PointOneK/Activities/Home/ProjectRowView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ProjectRowView: View {
-    var project: ProjectOld
+    @ObservedObject var project: ProjectOld
 
     var body: some View {
         VStack(alignment: .leading) {
